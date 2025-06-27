51ac5e8dd1b1c1da09da8de8abf40e0d8e267b23
dharwee <dharweedubae@gmail.com>
2025-06-27 23:25:54 +0530

    subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser} from "../controllers/user.controller.js";
+import {registerUser, loginUser, logoutUser, refreshAccessToken} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
 
 const router = Router()
 router.route("/register").post(
@@ -23,6 +22,9 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(
     verifyJWT, 
     logoutUser
+)
+router.route("/refresh-token").post(
+    refreshAccessToken,
 )
 
 export default router;
